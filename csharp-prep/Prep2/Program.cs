6a9804cba20f8ea174ade8f671e33070b7bc1341
wormy95 <woonern@gmail.com>
2023-04-22 11:04:32 +0800

    Grading system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your test score? ");
+        String scoreFromUser = Console.ReadLine();
+
+        int score = int.Parse(scoreFromUser);
+
+        int a = 90 ;
+        int b = 80 ;
+        int c = 70 ;
+        int d = 60 ;
+        int f = 60 ;
+
+        if (score >= a && score < 101)
+        {
+            Console.WriteLine("Congratulations, you passed with a Grade A!") ;
+        }
+
+        else if (score >= b && score < a)
+        {
+            Console.WriteLine("Congratulations, you passed with a Grade B!");
+        }
+
+        else if (score > c && score <b)
+        {
+            Console.WriteLine("Congratulations, you passed with a Grade C!") ;
+        }
+
+        else if (score > d && score <c)
+        {
+            Console.WriteLine("You did not pass, with a grade of D. Try again next time!");
+        }
+
+        else if (score < f)
+        {
+            Console.WriteLine("You did not pass, with a grade of F. Try again next time!");
+        }
+
+        else
+        {
+            Console.WriteLine("Please input a proper score");
+        }
     }
 }
