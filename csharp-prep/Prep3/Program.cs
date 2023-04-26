d7c0973ddbb0d55e73fec0799dd93f0e66fa5550
wormy95 <woonern@gmail.com>
2023-04-26 13:52:36 +0800

    Random number generator game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // String number = Console.ReadLine();
+        // int magicNumber = int.Parse(number);
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+    
+    int guess = -1;
+
+    // while (guess!= magicNumber)
+    do
+    {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > magicNumber)
+
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (guess < magicNumber)
+
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else if (guess == magicNumber)
+
+        {
+            Console.WriteLine("Congratulations, you guessed it!");
+        }
+
+    } while (guess != magicNumber);
+
+    
+
+
+
     }
 }
