7d69b11ad7bcee72d9e9228c159309d984497a39
wormy95 <woonern@gmail.com>
2023-04-26 16:36:30 +0800

    prep 4 code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,7 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+         List<int> numbers = new List<int>();
+
+         int userNumber = -1;
+
+         do
+         {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+         } while (userNumber != 0);
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of the list is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+
+        {
+            if(number > max) 
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The maximum number is {max}");
+
+
+
+
 
     }
    
