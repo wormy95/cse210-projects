81c4bfe4bff4a021878e1e97b87912f7a050a6eb
wormy95 <woonern@gmail.com>
2023-05-06 13:08:05 +0000

    commit

--- prepare/Learning02/job.cs
+++ prepare/Learning02/job.cs
@@ -1,20 +1,15 @@
+using System;
+
 public class Job
 {
-    private string _company;
-    private string _jobTitle;
-    private int _startYear;
-    private int _endYear;
-
-    public Job(string company, string jobTitle, int startYear, int endYear)
-    {
-        _company = company;
-        _jobTitle = jobTitle;
-        _startYear = startYear;
-        _endYear = endYear;
-    }
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
 
     public void Display()
     {
-        Console.WriteLine("{0} ({1}) {2}-{3}", _jobTitle, _company, _startYear, _endYear);
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
+
--- prepare/Learning02/resume.cs
+++ prepare/Learning02/resume.cs
@@ -1,24 +1,21 @@
+using System;
+
 public class Resume
 {
-    private string _personName;
-    private List<Job> _jobHistory;
+    public string _name;
 
-    public Resume(string personName)
-    {
-        _personName = personName;
-        _jobHistory = new List<Job>();
-    }
-
-    public void AddJob(Job job)
-    {
-        _jobHistory.Add(job);
-    }
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
 
     public void Display()
     {
-        Console.WriteLine("Resume for {0}:", _personName);
-        foreach (Job job in _jobHistory)
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
         {
+            // This calls the Display method on each job
             job.Display();
         }
     }
