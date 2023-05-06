13b44a0506cd031af39d260e7079f31b1434b71a
wormy95 <woonern@gmail.com>
2023-05-06 13:08:38 +0000

    commit2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle ="Software Engineer";
+        job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
