4ad4d369211437c2f45a2184da7439ae81e68278
wormy95 <woonern@gmail.com>
2023-05-15 14:25:01 +0000

    seperate into classes

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,10 +4,10 @@ using System.IO;
 
 class Program
 {
-    static List<string> journalEntries = new List<string>();
-
     static void Main(string[] args)
     {
+        Journal journal = new Journal();
+
         Console.WriteLine("Welcome to My Journal!");
 
         while (true)
@@ -25,16 +25,16 @@ class Program
             switch (option)
             {
                 case "1":
-                    WriteEntry();
+                    journal.WriteEntry();
                     break;
                 case "2":
-                    DisplayEntries();
+                    journal.DisplayEntries();
                     break;
                 case "3":
-                    SaveEntries();
+                    journal.SaveEntries();
                     break;
                 case "4":
-                    LoadEntries();
+                    journal.LoadEntries();
                     break;
                 case "5":
                     Console.WriteLine("Goodbye!");
@@ -45,16 +45,14 @@ class Program
             }
         }
     }
+}
 
-    static void WriteEntry()
+class Journal
+{
+    private List<Entry> journalEntries = new List<Entry>();
+    private List<string> prompts = new List<string>()
     {
-        List<string> prompts = new List<string>()
-        {
-            "What is your favorite color?",
-            "What is your favorite food?",
-            "What is your favorite book?",
-            "What is your favorite movie?",
-            "What is your favorite hobby?",
+         
             "Write about a time when you felt proud of yourself.",
             "What is something that you have been avoiding, and why?",
             "Describe a person who has influenced your life and how.",
@@ -65,19 +63,23 @@ class Program
             "Write about a time when you felt grateful for something or someone.",
             "What is a goal that you have set for yourself, and how do you plan to achieve it?",
             "Describe a challenge that you have overcome and what you learned from it."
-        };
+        // Add more prompts here...
+    };
 
-        // Select a random prompt
+    public void WriteEntry()
+    {
         Random random = new Random();
         string randomPrompt = prompts[random.Next(prompts.Count)];
 
         Console.WriteLine(randomPrompt);
-        string entry = Console.ReadLine();
+        string content = Console.ReadLine();
         DateTime currentTime = DateTime.Now;
-        journalEntries.Add($"{currentTime.ToString()} - {entry}");
+
+        Entry entry = new Entry(currentTime, content);
+        journalEntries.Add(entry);
     }
 
-    static void DisplayEntries()
+    public void DisplayEntries()
     {
         if (journalEntries.Count == 0)
         {
@@ -86,14 +88,14 @@ class Program
         else
         {
             Console.WriteLine("Journal Entries:");
-            foreach (string entry in journalEntries)
+            foreach (Entry entry in journalEntries)
             {
                 Console.WriteLine(entry);
             }
         }
     }
 
-    static void SaveEntries()
+    public void SaveEntries()
     {
         Console.WriteLine("Please enter a file name:");
         string fileName = Console.ReadLine();
@@ -101,16 +103,16 @@ class Program
 
         using (StreamWriter writer = new StreamWriter(fullPath))
         {
-            foreach (string entry in journalEntries)
+            foreach (Entry entry in journalEntries)
             {
-                writer.WriteLine(entry);
+                writer.WriteLine(entry.ToString());
             }
         }
 
         Console.WriteLine($"Entries saved to {fileName}.");
     }
 
-    static void LoadEntries()
+    public void LoadEntries()
     {
         Console.WriteLine("Please enter a file name:");
         string fileName = Console.ReadLine();
@@ -122,17 +124,54 @@ class Program
             return;
         }
 
-        List<string> entriesFromFile = new List<string>();
+        List<Entry> entriesFromFile = new List<Entry>();
         using (StreamReader reader = new StreamReader(fullPath))
         {
             string line;
             while ((line = reader.ReadLine()) != null)
             {
-                entriesFromFile.Add(line);
+                Entry entry = Entry.Parse(line);
+                if (entry != null)
+                {
+                    entriesFromFile.Add(entry);
+                }
             }
         }
 
         journalEntries = entriesFromFile;
         Console.WriteLine($"Entries loaded from {fileName}.");
+    }
+}
+
+class Entry
+{
+    public DateTime Time { get; set; }
+    public string Content { get; set; }
+
+    public Entry(DateTime time, string content)
+    {
+        Time = time;
+        Content = content;
+    }
+
+    public override string ToString()
+    {
+        return $"{Time.ToString()} - {Content}";
+    }
+
+    public static Entry Parse(string entryString)
+    {
+        string[] parts = entryString.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 2)
+        {
+            return null;
+        }
+
+        if (DateTime.TryParse(parts[0], out DateTime time))
+        {
+            return new Entry(time, parts[1]);
+        }
+
+        return null;
     }
 }
